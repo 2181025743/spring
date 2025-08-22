f4754df873138cb0d9d90d9684295e5babaf9cd8
yx <2181025743@qq.com>
2025-08-22 21:03:54 +0800

    1

---
+++ src/main/java/com/example/ioc/Teacher.java
@@ -0,0 +1,23 @@
+package com.example.ioc;
+
+public class Teacher {
+    private String name;
+    private String addr;
+
+    public Teacher() {
+    }                 // 必须要有无参构造器
+
+    // setter / getter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher { name='" + name + "', addr='" + addr + "' }";
+    }
+}
