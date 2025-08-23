a581d3d6a8d6230693056b8916e06293ff716572
yx <2181025743@qq.com>
2025-08-23 14:21:01 +0800

    day01作业

---
+++ day_02/src/main/java/yx/test/Test.java
@@ -0,0 +1,15 @@
+package yx.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import yx.injection.TeacherService;
+
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+        // System.out.println(context.getBean("teacherService"));
+        TeacherService service =
+                context.getBean(TeacherService.class);
+        System.out.println(service);
+    }
+}
